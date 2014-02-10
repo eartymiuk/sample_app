da1226f5b3ad12be378e9dabfc78462423e84058
Erik Artymiuk <eartymiuk@wisc.edu>
2014-02-10 10:35:20 -0600

    added users

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140208160139) do
+ActiveRecord::Schema.define(version: 20140210162540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: true do |t|
     t.string   "name"
-    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
