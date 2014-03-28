8aac2955ae18b0e162383c266980d770ff2f8d5c
Erik Artymiuk <eartymiuk@wisc.edu>
2014-03-28 09:54:35 -0500

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   get "static_pages/home"
   get "static_pages/faq"
   get "static_pages/about"
   get "static_pages/blog"
   
   root 'static_pages#home'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
+
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
