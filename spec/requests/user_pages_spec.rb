68cf18e35352e27a7d953df680d17c3e55104f74
Erik Artymiuk <eartymiuk@wisc.edu>
2014-03-28 11:47:38 -0500

    Finish Exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,7 +10,7 @@ describe "User pages" do
   describe "signup page" do
     before { visit signup_path }
 
-    it { should have_content('Sign up') }
+    it { should have_content('Sign up') } 
     it { should have_title(full_title('Sign up')) }
   end
 
@@ -45,16 +45,11 @@ describe "User pages" do
 
         it { should have_link('Sign out') }
         it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') } 
+        it { should have_success_message('Welcome') } 
 
       end   
 
-      before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
-      end
+      before { fillin_sample_user_info }
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
