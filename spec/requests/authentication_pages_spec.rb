050eaa6a9608bbbf254dc5a45e5ee06e67ef67c2
Erik Artymiuk <eartymiuk@wisc.edu>
2014-04-07 16:55:51 -0500

    Finish Ch 9 Exercises

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,6 +4,12 @@ describe "Authentication" do
 
   subject { page }
 
+  describe "not signed in" do
+      before { visit signin_path }
+      it { should_not have_link('Profile') }
+      it { should_not have_link('Settings') }
+    end
+
   describe "signin" do
     before { visit signin_path }
 
@@ -41,6 +47,17 @@ describe "Authentication" do
 
 describe "authorization" do
 
+  describe "for admin users" do
+    let(:admin) { FactoryGirl.create(:admin) }
+    before { sign_in admin, no_capybara: true }
+
+    describe "submitting a DELETE request to the Users#destory action for itself" do
+      before { delete user_path(admin) }
+      specify { expect(response).to redirect_to(root_url) }
+    end 
+  end
+  
+
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
 
@@ -56,6 +73,20 @@ describe "authorization" do
 
           it "should render the desired protected page" do
             expect(page).to have_title('Edit user')
+          end
+
+          describe "when signing in again" do
+            before do
+              click_link "Sign out"
+              visit signin_path
+              fill_in "Email",    with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
+            end
+
+            it "should render the default (profile) page" do
+              expect(page).to have_title(user.name)
+            end
           end
         end
       end
